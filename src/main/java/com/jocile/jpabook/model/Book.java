b51c24aa7a66b4681ba57653004f86fe4b59bbee
Jocilé Serra <jocile@gmail.com>
2023-07-05 19:36:12 -0300

    model: Adicionando entidade Book com anotations

---
+++ src/main/java/com/jocile/jpabook/model/Book.java
@@ -0,0 +1,37 @@
+package com.jocile.jpabook.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Book {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String author;
+
+    public Book() {
+        id = -1;
+        title = "";
+        author = "";
+    }
+
+    public Book(Integer id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+    
+    public Book(String title) {
+        id = -1;
+        this.title = title;
+        author = "";
+    }
+}
