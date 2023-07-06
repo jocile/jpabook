eb951b9c6a0dbc66fbf9cf50853bedab47795517
Jocilé Serra <jocile@gmail.com>
2023-07-06 00:11:45 -0300

    Main: EntityManagerFactory e EntityManager

--- src/main/java/com/jocile/jpabook/Jpabook.java
+++ src/main/java/com/jocile/jpabook/Jpabook.java
@@ -1,16 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.jocile.jpabook;
 
-/**
- *
- * @author joc
- */
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Jpabook {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager entityManager = factory.createEntityManager();
+        
+        // todo: codigo
+        
+        entityManager.close();
+        factory.close();
     }
 }
