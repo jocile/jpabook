7295a825b63c6cddce688bc1a358a66f6d7d1f04
Jocilé Serra <jocile@gmail.com>
2023-07-06 17:42:02 -0300

    Teste: Funcao para encontrar um registro por critério especifico

--- README.md
+++ README.md
@@ -15,4 +15,5 @@ Exemplo de Persistencia com JPA e Hibernate na prática das aulas
 
 [![JLPS-13 - Persistencia - JPAHibernate na prática](http://img.youtube.com/vi/9TSSCCNXLJw/0.jpg)](https://www.youtube.com/watch?v=9TSSCCNXLJw)
 
-[Documentação Hibernate - EntityManager](https://docs.jboss.org/hibernate/jpa/2.2/api/javax/persistence/EntityManager.html)
+- [Documentação Hibernate - EntityManager](https://docs.jboss.org/hibernate/jpa/2.2/api/javax/persistence/EntityManager.html)
+- [Types of JPA Queries](https://www.baeldung.com/jpa-queries)
--- src/main/java/com/jocile/jpabook/Jpabook.java
+++ src/main/java/com/jocile/jpabook/Jpabook.java
@@ -1,9 +1,12 @@
 package com.jocile.jpabook;
 
 import com.jocile.jpabook.model.Book;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 public class Jpabook {
 
@@ -37,9 +40,36 @@ public class Jpabook {
         entityManager.getTransaction().begin();
         entityManager.remove(livro);
         entityManager.getTransaction().commit();
-        */
+        */        
         
-        entityManager.close();
+        // Encontar um registro por um critério específico
+        Book livroAuthor = findByAuthor(entityManager, "Deitel");
+        System.out.println(livroAuthor.toString());
+
+        entityManager.close();        
         factory.close();
+    }
+    
+    public static Book findByAuthor(EntityManager entityManager, String author){
+        /*
+        String jpql = " SELECT b.id, b.title, b.author "+
+                      " FROM Book b "+
+                      " WHERE b.author = :author";
+        */
+        String jpql = " SELECT b FROM Book b "+
+                      " WHERE b.author = :author";
+        //Query query = entityManager.createQuery(jpql);
+        //query.setParameter("author", author);
+        //TypedQuery typedQuery = entityManager.createQuery(jpql, Book.class);
+        TypedQuery<Book> typedQuery = entityManager.createQuery(jpql, Book.class);
+        typedQuery.setParameter("author", author);
+        
+        //List<Book> lst = (Book) query.getResultList();
+        //List<Book> lst = typeQuery.getResultList();
+        Book livro = (Book) typedQuery.getSingleResult();
+
+        entityManager.close();
+        //return lst.get(0);
+        return livro;
     }
 }
